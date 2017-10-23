bddcdecb478e275f21423b2c63e9b54806649857
Erich Nachbar <erich@stuffylab.com>
2017-10-22 22:29:04 -0700

    still wonky

--- wagonV2.ino
+++ wagonV2.ino
@@ -19,10 +19,10 @@
 #define led 13
 
 SoftwareSerial9 mySerialL(RXL, TXL);
-SoftwareSerial9 mySerialR(RXR, TXR);
-RCReceive rcReceiver;
+//SoftwareSerial9 mySerialR(RXR, TXR);
+//RCReceive rcReceiver;
 
-const byte PIN_RC = 2;
+const byte PIN_RC = 12;
 int i = 0;
 byte value = 127;
 signed int mapVal = 0;
@@ -30,28 +30,28 @@ signed int mapVal = 0;
 void setup() {
   pinMode(led, OUTPUT);
   mySerialL.begin(26315);
-  mySerialR.begin(26315);
+  //  mySerialR.begin(26315);
   Serial.begin(115200);
-  rcReceiver.attach(PIN_RC);
-  //  rcReceiver.attachInt(PIN_RC); // doesn't give stable values
-
+  //  rcReceiver.attach(PIN_RC);
 }
 
 void loop() {
 
-  if (rcReceiver.hasNP()) {
-    rcReceiver.poll();
-    value = rcReceiver.getValue();
-  }
+  //  if (rcReceiver.hasNP()) {
+  //    rcReceiver.poll();
+  //    value = rcReceiver.getValue();
+  //  }
 
   //    signed int mapVal = map(value, 32, 225, -1550, 1550);
 
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
 
   if (i % 10 == 0) {
-    rcReceiver.poll();
-    value = rcReceiver.getValue();
-    mapVal = map(value, 0, 250, -500, 500);
+    //    rcReceiver.poll();
+//    value = rcReceiver.getValue();
+//    mapVal = map(value, 0, 250, -500, 500);
+
+    long value = pulseIn(PIN_RC, HIGH);
 
     Serial.print("mapVal ");
     Serial.print(mapVal);
@@ -68,9 +68,9 @@ void loop() {
   //delay(50);
   digitalWrite(led, LOW);   // turn the LED on (HIGH is the voltage level)
 
-  if (!rcReceiver.hasNP() && rcReceiver.hasError()) {
-    Serial.println("****** ERROR");;
-  }
+//  if (!rcReceiver.hasNP() && rcReceiver.hasError()) {
+//    Serial.println("****** ERROR");;
+//  }
 }
 
 void sendToMotors(signed int motorL, signed int motorR) {
@@ -82,12 +82,12 @@ void sendToMotors(signed int motorL, signed int motorR) {
   mySerialL.write9((motorL >> 8) & 0xFF);
   mySerialL.write9(85);
 
-  mySerialR.write9(256);
-  mySerialR.write9(motorR & 0xFF);
-  mySerialR.write9((motorR >> 8) & 0xFF);
-  mySerialR.write9(motorR & 0xFF);
-  mySerialR.write9((motorR >> 8) & 0xFF);
-  mySerialR.write9(85);
+  //  mySerialR.write9(256);
+  //  mySerialR.write9(motorR & 0xFF);
+  //  mySerialR.write9((motorR >> 8) & 0xFF);
+  //  mySerialR.write9(motorR & 0xFF);
+  //  mySerialR.write9((motorR >> 8) & 0xFF);
+  //  mySerialR.write9(85);
 
 }
 
